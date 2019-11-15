ea6b976c6ce1382f3aebb3dc77c7e91f3261928c
Kenny Kam <KlaudeTsing@outlook.com>
2019-11-15 21:23:00 +0800

    主干00

---
+++ main/java/cn/gky/main/Hellow00.java
@@ -0,0 +1,11 @@
+package cn.gky.main;
+
+/**
+ * @Author Kenny Kam
+ * @Date 2019/11/15 21:16
+ */
+public class Hellow00 {
+    public static void main(String[] args) {
+        System.out.println("hellow");
+    }
+}
